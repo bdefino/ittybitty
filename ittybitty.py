44d6677b97281230b989c31ff1899425048c087a
hiten2 <hiten@riseup.net>
2018-10-02 20:11:06 -0400

    init

---
+++ ittybitty.py
@@ -0,0 +1,62 @@
+# Copyright (C) 2018 Bailey Defino
+# <https://bdefino.github.io>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__doc__ = "itty bitty ops on bits"
+
+class BitAccess:
+    """faculty for easy manipulation of a bit array"""
+    
+    def __init__(self, octets = ''):
+        self._iterator_index = 0
+        
+        if not isinstance(octets, bytearray):
+            octets = bytearray(octets)
+        self.octets = octets
+
+    def clear(self):
+        """zero out the bits"""
+        for i in range(self.__len__()):
+            self.__setitem__(i, 0)
+    
+    def __getitem__(self, i):
+        return (self.octets[i / 8] >> (i % 8)) & 1
+
+    def __iter__(self):
+        return self
+    
+    def __len__(self):
+        return len(self.octets) * 8
+
+    def next(self):
+        if self._iteration_index >= len(self.octets) << 3:
+            raise StopIteration()
+        bit = (self.octets[self._iteration_index / 8]
+            >> (self._iteration_index % 8)) & 1
+        self._iteration_index += 1
+        return bit
+    
+    def __setitem__(self, i, high):
+        assert high in (0, 1), "high must evaluate to either 0 or 1"
+        shift = i % 8
+        octet = self.octets[i / 8] & ~(1 << shift) # isolate index
+        self.octets[i / 8] = octet | (high << shift)
+
+    def __str__(self):
+        return str(self.octets)
+
+    def value(self, i):
+        """return the value of an indexed bit within its corresponding octet"""
+        return self.octets[i / 8] & (1 << (i % 8))
